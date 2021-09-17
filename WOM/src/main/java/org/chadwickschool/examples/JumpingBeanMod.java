14ffb21b5ed7616d99fdfab159e85594bd8f180d
Joseph Collard <jcollard@users.noreply.github.com>
2021-09-16 19:52:49 -0700

    feat: Add JumpingBeanMod and ModPackExample.

---
+++ WOM/src/main/java/org/chadwickschool/examples/JumpingBeanMod.java
@@ -0,0 +1,51 @@
+package org.chadwickschool.examples;
+
+import com.worldsofminecraft.mod.MinecraftMod;
+import com.worldsofminecraft.mod.MinecraftMod.Builder;
+import com.worldsofminecraft.mod.QuickMod;
+import com.worldsofminecraft.mod.item.QuickFood;
+import com.worldsofminecraft.mod.item.tab.ItemTab;
+import com.worldsofminecraft.mod.potion.Effect;
+import com.worldsofminecraft.mod.potion.Effect.Type;
+
+//TODO: Uncomment this line if you'd like to use this mod
+//@Mod(JumpingBeanMod.MODID)
+public class JumpingBeanMod extends QuickMod {
+
+	public static final String MODID = "jumping_beans";
+
+	@Override
+	public Builder getBuilder() {
+		String authors = "Joseph Collard";
+		String modName = "Jumping Beans";
+		MinecraftMod.Builder builder = MinecraftMod.getBuilder(authors, modName, MODID);
+
+		// Sets the logo that appears on the Mods page in Minecraft
+		builder.logoFile("assets/common/jumping_beans.png");
+		// Sets the description that appears on this mod in the Mods page
+		builder.description("This mod adds a magic bean that lets the player jump really high!");
+
+		// Create a QuickFood specifying its in game name and the texture to use
+		QuickFood jumpingBean = new QuickFood("Jumping Beans", "assets/common/jumping_beans.png");
+
+		// Add jumpingBean to the Food tab
+		jumpingBean	.getProperties()
+					.tab(ItemTab.FOOD);
+
+		// Specify how many food points the jumping bean should restore
+		jumpingBean.foodPoints(2);
+
+		// Create an Effect that will be applied after the beans are consumped
+		Effect jumpingBeanEffect = new Effect(Type.JUMP_BOOST)	.level(2)
+																.seconds(20);
+
+		// Add the Effect to the Jumping Bean
+		jumpingBean.addEffect(jumpingBeanEffect);
+
+		// Add the jumping bean to the builder
+		builder.addItem(jumpingBean);
+
+		return builder;
+	}
+
+}
---
+++ WOM/src/main/java/org/chadwickschool/examples/ModPackExample.java
@@ -0,0 +1,21 @@
+package org.chadwickschool.examples;
+
+import com.worldsofminecraft.mod.ModPack;
+
+public class ModPackExample {
+
+	public static void main(String[] args) {
+		// A ModPack allows you to put multiple mods into the same build.
+		// Use the ModPack constructor to create a new one.
+		ModPack pack = new ModPack();
+
+		// Next, you construct and add each mod you would like to include
+		// pack.addMod(new BananaMod());
+		pack.addMod(new JumpingBeanMod());
+
+		// Finally, you call the build() method for the ModPack
+		// This will build all of the added mods.
+		pack.build();
+	}
+
+}
